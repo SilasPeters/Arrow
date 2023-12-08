105fcebe7cbd7101c471bd022a3836b020704ece
Silas Peters <silaspeters03@gmail.com>
2023-12-08 22:29:33 +0100

    Exercise 2

--- src/Model.hs
+++ src/Model.hs
@@ -10,4 +10,23 @@ data Token = TArrow | TDot | TComma
   deriving Show
 
 -- Exercise 2
-data Program = Program deriving Show
+newtype Program = Program [Rule]
+  deriving Show
+
+data Rule = Rule String [Command]
+  deriving Show
+
+data Command = CGo | CTake | CMark | CNothing
+             | CTurn Dir | CCase Dir [Alt]
+             | CIdent String
+  deriving Show
+
+data Dir = Left | Right | Front
+  deriving Show
+
+data Alt = Alt Pat [Command]
+  deriving Show
+
+data Pat = PEmpty | PLambda | PDebris | PAstroid | PBoundary | PUnderscore
+  deriving Show
+
